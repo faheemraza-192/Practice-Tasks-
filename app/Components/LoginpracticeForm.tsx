52236c526ec09b056d3f1b13e48cc9f631c76d95
Faheem Raza <faheem39794@gmail.com>
2026-01-16 19:18:20 +0500

    formik connected with form and inputs

--- app/Components/LoginpracticeForm.tsx
+++ app/Components/LoginpracticeForm.tsx
@@ -1,25 +1,38 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, useFormik } from "formik";
 import * as Yup from "yup";  
 function LoginpracticeForm() {
+
+    // =============  first step: (1)  => FormiK =============
+    const formik=useFormik({
+initialValues:{
+    name:"",
+    email:"",
+    password:"",
+    confirmpassword:""
+},
+onSubmit:(values, actions)=>{
+
+}
+    })
   return (
     <div>
         <div className="mt-5 pt-5 mb-3"   style={{textAlign:"center"}}>
-        <form action="" className="border" style={{width:"fit-content",justifySelf:"center",padding:"15px"}}> 
+        <form onSubmit={formik.handleSubmit} action="" className="border" style={{width:"fit-content",justifySelf:"center",padding:"15px",borderRadius:"10px"}}> 
           <div >
-            <input className="border p-2 m-2" type="text" name="name" placeholder="Your Name  here . . . " />
+            <input className="border p-2 m-2" value={formik.values.name} type="text" name="name" placeholder="Your Name  here . . . " />
             <p></p>
           </div>
           <div>
-            <input className="border p-2 m-2" type="email" name="email" placeholder="Your Email here . . . " />
+            <input className="border p-2 m-2" value={formik.values.email} type="email" name="email" placeholder="Your Email here . . . " />
           </div>
           <p></p>
           <div>
-            <input className="border p-2 m-2" type="password" name="password" placeholder="Your Password here . . . " />
+            <input className="border p-2 m-2" value={formik.values.password} type="password" name="password" placeholder="Your Password here . . . " />
           </div>
           <p></p>
           <div>
-            <input className="border p-2 m-2" type="password" name="confirmpassword" placeholder="Confirm Your Password . . . " />
+            <input className="border p-2 m-2" value={formik.values.confirmpassword} type="password" name="confirmpassword" placeholder="Confirm Your Password . . . " />
           </div>
           <p></p>
         </form>
